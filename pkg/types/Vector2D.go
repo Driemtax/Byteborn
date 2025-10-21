3ee421d98ce0056999fbe91117044c61a0000d93
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2025-10-21 08:24:38 +0200

    feat: Added pkg util functions for positions

--- pkg/types/Vector2D.go
+++ pkg/types/Vector2D.go
@@ -4,3 +4,11 @@ type Vector2D struct {
 	X float64
 	Y float64
 }
+
+// Equals: Checks if the vector is equal to another vector
+func (v Vector2D) Eq(otherVector Vector2D) bool {
+	if v.X == otherVector.X && v.Y == otherVector.Y {
+		return true
+	}
+	return false
+}
