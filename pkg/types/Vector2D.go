e5c60e4b7192e6abce5b61c2215dcc2d362930eb
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2025-10-21 08:24:38 +0200

    feat: Added pkg util functions for positions

--- pkg/types/Vector2D.go
+++ pkg/types/Vector2D.go
@@ -11,3 +11,11 @@ func NewVector2D(x, y float64) Vector2D {
 		Y: y,
 	}
 }
+
+// Equals: Checks if the vector is equal to another vector
+func (v Vector2D) Eq(otherVector Vector2D) bool {
+	if v.X == otherVector.X && v.Y == otherVector.Y {
+		return true
+	}
+	return false
+}
---
+++ pkg/util/positions.go
@@ -0,0 +1,25 @@
+package util
+
+import "github.com/Driemtax/Byteborn/pkg/types"
+
+func IsInBounds(pos types.Vector2D, minX, minY, maxX, maxY float64) bool {
+	if pos.X >= minX && pos.X <= maxX && pos.Y >= minY && pos.Y <= maxY {
+		return true
+	}
+	return false
+}
+
+func Clamp(pos types.Vector2D, minX, minY, maxX, maxY float64) types.Vector2D {
+	if pos.X < minX {
+		pos.X = minX
+	} else if pos.X > maxX {
+		pos.X = maxX
+	}
+
+	if pos.Y < minY {
+		pos.Y = minY
+	} else if pos.Y > maxY {
+		pos.Y = maxY
+	}
+	return pos
+}
