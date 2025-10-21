b26fcc7b67e988a5bd6b581337eef02f9bbdcf56
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2025-10-21 08:28:07 +0200

    feat(pkg): Added alias Vec2 for Vector2D

--- pkg/types/Vector2D.go
+++ pkg/types/Vector2D.go
@@ -5,6 +5,8 @@ type Vector2D struct {
 	Y float64
 }
 
+type Vec2 = Vector2D
+
 // Equals: Checks if the vector is equal to another vector
 func (v Vector2D) Eq(otherVector Vector2D) bool {
 	if v.X == otherVector.X && v.Y == otherVector.Y {
